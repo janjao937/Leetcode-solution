6abd9a7b79f9a9a9bd55a974d9f3a002b0e30961
Boonyakit <61728559+janjao937@users.noreply.github.com>
2026-08-06 04:55:02 +0700

    Time: 1 ms (93.36%), Space: 53.3 MB (31.81%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cs
+++ 0002-add-two-numbers/0002-add-two-numbers.cs
@@ -11,21 +11,30 @@
  */
 public class Solution {
     public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
-        ListNode current = dummy;
+
+        var dummy = new ListNode();
+        var tail = dummy;
         int carry = 0;
 
-        while(l1 != null || l2 != null || carry != 0){
-            int val1 = (l1 != null) ? l1.val : 0;
-            int val2 = (l2 != null) ? l2.val : 0;
 
-            int sum = val1 + val2 + carry;
+        while (l1 != null || l2 != null || carry > 0) {
+            int sum = carry;
+
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+   
+            tail.next = new ListNode(sum % 10);
+            tail = tail.next;
+
             carry = sum / 10;
-            current.next = new ListNode(sum % 10);
-            current = current.next;
-
-            if (l1 != null) l1 = l1.next;
-            if (l2 != null) l2 = l2.next;
         }
 
         return dummy.next;
